55aa36cf3b7ce75235e5a92e3493e0b6bfb6e41b
Aaron Zachariah <azachariah@ipvideocorp.com>
2023-10-20 22:07:52 -0400

    snake game basic implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ package main.java;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        
+        new GameFrame();
+        
     }
 
 }
